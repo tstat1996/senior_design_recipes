4c57f6647cb1cd826b5cba3162862862bdc2db18
Dorothy Chang <dorchang@seas.upenn.edu>
2018-11-11 19:53:51 -0500

    form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Form from './Form';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Welcome to Penn Course Recs
-          </p>
-        </header>
+      <div className="homepage">
+        <p>
+          Welcome to Penn Course Recs
+        </p>
+        <Form />
       </div>
     );
   }
